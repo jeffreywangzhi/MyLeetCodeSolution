2399bebd7a71cf26423a7e596f2195f544df595e
JeffreyWangz <jeffrey02120212@gmail.com>
2024-05-28 17:27:47 +0800

    added 47.PermutationsII solution

---
+++ 47.PermutationsII.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        int[] visited = new int[nums.length];
+        List<Integer> cur = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(nums, 0, visited, cur, res);
+        return res;
+    }
+    private void dfs(int[] nums, int level, int[] visited, List<Integer> cur, List<List<Integer>> res) {
+        if ( level == nums.length ) { res.add(new ArrayList<>(cur)); return; }
+        for ( int i = 0; i < nums.length; i++ ) {
+            if ( visited[i] == 1 ) continue;
+            if ( i > 0 && nums[i] == nums[i-1] && visited[i-1] == 0 ) continue;
+            visited[i] = 1;
+            cur.add(nums[i]);
+            dfs(nums, level+1, visited, cur, res);
+            cur.remove(cur.size()-1);
+            visited[i] = 0;
+        }
+    }
+}
