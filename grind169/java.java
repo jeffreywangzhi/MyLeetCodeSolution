0b3153726af56bbc2770d3a147faf201c1a2b5c6
JeffreyWangz <jeffrey02120212@gmail.com>
2024-06-25 17:47:37 +0800

    added grind169-1 1.TwoSum

--- grind169/java.java
+++ grind169/java.java
@@ -1,4 +1,16 @@
-public class java {
-    public static void main(String[] args){
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] res = new int[2];
+        for ( int i = 0; i < nums.length; i++ ) {
+            if ( !map.containsKey(nums[i]) )
+                map.put(target - nums[i], i);
+            else {
+                res[0] = map.get(nums[i]);
+                res[1] = i;
+                break;
+            }
+        }
+        return res;
     }
 }
