46fcc3d2f805128ddc4c49b2581e8e51d35ae3d1
JeffreyWangz <jeffrey02120212@gmail.com>
2024-07-23 23:33:12 +0800

    added grind169-70 54.SpiralMatrix solution

---
+++ grind169/week3/54.SpiralMatrix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int up = 0, down = matrix.length-1, left = 0, right = matrix[0].length-1;
+        while ( true ) {
+            for ( int i = left; i <= right; i++ ) res.add(matrix[up][i]);
+            if ( ++up > down ) break;
+            for ( int i = up; i <= down; i++ ) res.add(matrix[i][right]);
+            if ( --right < left ) break;
+            for ( int i = right; i >= left; i-- ) res.add(matrix[down][i]);
+            if ( --down < up ) break;
+            for ( int i = down; i >= up; i-- ) res.add(matrix[i][left]);
+            if ( ++left > right ) break;
+        }
+        return res;
+    }
+}
