af9dc1b3d557f60111493673b21e163a31d859f5
JeffreyWangz <jeffrey02120212@gmail.com>
2024-07-24 00:48:18 +0800

    added grind169-74 62.UniquePaths solution

---
+++ grind169/week3/62.UniquePaths.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        for ( int i = 0; i < m; i++ ) {
+            for ( int j = 0; j < n; j++ ) {
+                if ( i == 0 || j == 0) dp[j] = 1;
+                else dp[j] += dp[j-1];
+            }
+        }
+        return dp[n-1];
+    }
+}
