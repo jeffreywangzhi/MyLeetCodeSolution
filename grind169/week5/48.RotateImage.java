38503974cb35580e7e83876633f8a4331bb49ab9
JeffreyWangz <jeffrey02120212@gmail.com>
2024-09-03 15:30:17 -0400

    added grind169-122 48.RotateImage solution

---
+++ grind169/week5/48.RotateImage.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for ( int i = 0; i < matrix.length; i++ ) {
+            for ( int j = 0; j < i; j++ ) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for ( int i = 0; i < matrix.length; i++ ) {
+            for ( int j = 0, k = matrix[0].length-1; j < matrix[0].length/2; j++, k-- ) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][k];
+                matrix[i][k] = temp;
+            }
+        }
+    }
+}
