6e9029fca961161406ac750cba222ccb784590ea
JeffreyWangz <jeffrey02120212@gmail.com>
2024-09-10 17:06:39 -0400

    added grind169-132 73.SetMatrixZeroes solution

---
+++ grind169/week6/73.SetMatrixZeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean flag = false;
+        for ( int i = 0; i < matrix.length; i++ ) {
+            if ( matrix[i][0] == 0 ) flag = true;
+            for ( int j = 1; j < matrix[0].length; j++ ) {
+                if ( matrix[i][j] == 0 ) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for ( int i = matrix.length-1; i >= 0; i-- ) {
+            for ( int j = matrix[0].length-1; j >= 1; j-- ) {
+                if ( matrix[i][0] == 0 || matrix[0][j] == 0 ) matrix[i][j] = 0;
+            }
+            if ( flag ) matrix[i][0] = 0;
+        }
+    }
+}
